d8dfc0febeecbac0cf7ce50de519ce737b430b8a
javeryking <jaking@paloaltonetworks.com>
2024-09-11 16:43:49 +0000

    initial commit

--- authenticateCloudAndRuntime.py
+++ authenticateCloudAndRuntime.py
@@ -1,8 +1,8 @@
 import json, requests
 
 url = "https://api0.prismacloud.io/login"
-username = "" #Access Key ID
-password = "" #Secret Key
+username = "3d852ead-cf40-4424-b9f3-5ed3a76e74ae" #Access Key ID
+password = "Hkb9dM2qj0bqaa2abaMYyyQDVj0=" #Secret Key
 
 payload = json.dumps({
     "username": f"{username}",
